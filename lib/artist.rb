5984eaa75741e91b0a25db37a199bf9e01b5a455
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 19:59:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def songs
+  end
+  
+  def add_song(song)
+  end
+  
+  def add_song_by_name(name)
+    
+  end
+
+
+end
