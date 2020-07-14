3e306351d18b1cc878a45912ab6d16746b9b3c22
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 20:17:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,21 @@
 class Author
   
+  postcount = 0
+  
   attr_accessor :name
   
   def initialize(name)
     @name = name
+  end
+  
+  def posts
+    Post.all.select do |posts| 
+      posts.name == self
+    end
+  end
+  
+  def add_post(post)
+    post.author = self
   end
   
 end
