d67fbbd6672381da195d7fa19f0912320660f347
Hope Gipson <gipson.hope@gmail.com>
2020-07-14 20:11:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  @@counter = 0
   
   def initialize(name)
     @name = name
@@ -13,17 +14,17 @@ class Artist
   
   def add_song(song)
     song.artist = self
+    counter += 1
   end
   
   def add_song_by_name(name)
     song1 = Song.new(name)
     add_song(song1)
+    counter += 1
   end
   
   def song_count
-    counter = 0
-    Song.all.each do |song|
-      counter += 1
+    
     end
   end
 
